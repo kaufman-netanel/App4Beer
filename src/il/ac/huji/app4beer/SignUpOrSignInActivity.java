cc317b09d780edc8d1d3edaaf1ce8051d8feb7d0
Kaufman Netanel <kaufman.netanel@gmail.com>
2013-09-26 23:26:23 +0300

    sign in or up parse user

--- src/il/ac/huji/app4beer/SignUpOrSignInActivity.java
+++ src/il/ac/huji/app4beer/SignUpOrSignInActivity.java
@@ -1,33 +1,95 @@
 package il.ac.huji.app4beer;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class SignUpOrSignInActivity extends Activity {
 
-    private final class SignAction implements OnClickListener {
-    	private Boolean _signin;
+	private Button _signInButton;
+   	private Button _signUpButton;
+   	private TextView _errmsg;
+
+   	private final class SignAction implements OnClickListener {
     	
+    	private Boolean _signin;
+       	
     	public SignAction(Boolean signin) {
     		_signin = signin;
     	}
     	
+    	private void onDone(ParseUser user, ParseException e, String what) {
+ 			
+  			if (user != null || e == null) {
+  				_errmsg.setText(what);
+	    		  setResult(RESULT_OK);
+	    		  finish();
+			} else {
+				switch(e.getCode()){
+				case ParseException.USERNAME_TAKEN:
+					_errmsg.setText("Sorry, this username has already been taken.");
+					break;
+				case ParseException.USERNAME_MISSING:
+					_errmsg.setText("Sorry, you must supply a username to register.");
+					break;
+				case ParseException.PASSWORD_MISSING:
+					_errmsg.setText("Sorry, you must supply a password to register.");
+					break;
+				case ParseException.OBJECT_NOT_FOUND:
+					_errmsg.setText("Sorry, those credentials were invalid.");
+					break;
+				default:
+					_errmsg.setText(e.getLocalizedMessage());
+					break;
+				}
+				_signInButton.setEnabled(true);
+				_signUpButton.setEnabled(true);
+			}
+
+    	}
+    	
 		public void onClick(View v) {
-			Intent result = new Intent();
+			
 			EditText phonenumber = (EditText)findViewById(R.id.phonenumber);
-			result.putExtra("phonenumber", phonenumber.getText().toString());
 			EditText displayname = (EditText)findViewById(R.id.displayname);
-			result.putExtra("displayname", displayname.getText().toString());
-			result.putExtra("signin", _signin);
-			setResult(_signin ? RESULT_OK : RESULT_CANCELED, result);
-			finish();
+			if(phonenumber.getText().length() == 0 || displayname.getText().length() == 0) {
+			      return;
+			}
+
+			v.setEnabled(false);
+			ParseUser user = new ParseUser();
+			user.setUsername(displayname.getText().toString());
+			user.setPassword(phonenumber.getText().toString());
+			
+			_errmsg.setText("Wait for it...");
+			if (_signin) {
+				ParseUser.logInInBackground(displayname.getText().toString(), 
+						phonenumber.getText().toString(), 
+						new LogInCallback() {
+					@Override
+					public void done(ParseUser user, ParseException e) {
+						onDone(user, e, "LOGGED");
+					}
+				});
+			} else {
+				user.signUpInBackground(new SignUpCallback(){
+					@Override
+					public void done(ParseException e) {
+						onDone(null, e, "SIGNED UP");
+					}
+				});
+			}
 		}
 	}
 
@@ -37,12 +99,14 @@ public class SignUpOrSignInActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		
-		Button signInButton = (Button)findViewById(R.id.signin);
-		signInButton.setOnClickListener(new SignAction(true));	 
+		_signInButton = (Button)findViewById(R.id.signin);
+		_signInButton.setOnClickListener(new SignAction(true));	 
 
-		Button signUpButton = (Button)findViewById(R.id.signup);
-		signUpButton.setOnClickListener(new SignAction(false));	 
-    }
+		_signUpButton = (Button)findViewById(R.id.signup);
+		_signUpButton.setOnClickListener(new SignAction(false));	 
+    
+		_errmsg = (TextView)findViewById(R.id.errmsg);
+	}
 
 
     @Override
