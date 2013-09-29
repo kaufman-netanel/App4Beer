8194ead7493dc5ad6173cba0c74e7f43d519af81
Kaufman Netanel <kaufman.netanel@gmail.com>
2013-09-30 00:50:32 +0300

    populate eventmanager fields

--- src/il/ac/huji/app4beer/EventManager.java
+++ src/il/ac/huji/app4beer/EventManager.java
@@ -1,5 +1,8 @@
 package il.ac.huji.app4beer;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import il.ac.huji.app4beer.DAL.Event;
 import il.ac.huji.app4beer.DAL.DAL;
 import android.os.Bundle;
@@ -13,6 +16,9 @@ public class EventManager extends Activity {
 	
 	private TextView _eventTitleTextView;
 	private TextView _eventDescriptionTextView;
+	private TextView _eventLocationTextView;
+	private TextView _eventDateTextView;
+	private TextView _eventTimeTextView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +29,17 @@ public class EventManager extends Activity {
 		
 		_eventTitleTextView = (TextView)findViewById(R.id.EventManager_title);
 		_eventDescriptionTextView = (TextView)findViewById(R.id.EventManager_Desc);
+		_eventLocationTextView = (TextView)findViewById(R.id.EventManager_location);
+		_eventDateTextView = (TextView)findViewById(R.id.EventManager_date);
+		_eventTimeTextView = (TextView)findViewById(R.id.EventManager_time);
 		
 		_eventTitleTextView.setText(_event.get_title());
 		_eventDescriptionTextView.setText(_event.get_description());
+		_eventLocationTextView.setText(_event.get_location());
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(_event.get_date());
+		_eventDateTextView.setText(String.format("%02d / %02d / %04d", calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH), calendar.get(Calendar.YEAR)));
+		_eventTimeTextView.setText(String.format("%02d : %02d", calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE)));
 		
 		
 	}
